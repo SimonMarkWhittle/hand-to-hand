a5d0e3964ab49522eaabd5266b3023a6a8ee45e4
SimonMarkWhittle <smw2ed@virginia.edu>
2018-07-14 21:52:26 -0400

    States enums

--- Hand to Hand/Assets/Resources/Scripts/States.cs
+++ Hand to Hand/Assets/Resources/Scripts/States.cs
@@ -2,6 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Action_States {
+    Up_Idle, Low_Idle, Air_Idle, Moving_Forward, Moving_Backward,
+    Up_LightPunch, Low_LightPunch, Air_LightPunch,
+    Up_HeavyPunch, Low_HeavyPunch, Air_HeavyPunch,
+    Up_LightKick, Low_LightKick, Air_LightKick,
+    Up_HeavyKick, Low_HeavyKick, Air_HeavyKick,
+    Stun, Jump, Grapple
+};
+
+public enum Execution_States {
+    Preparing, Executing, Recoiling
+};
+
 public class States : MonoBehaviour {
 
 	// Use this for initialization
