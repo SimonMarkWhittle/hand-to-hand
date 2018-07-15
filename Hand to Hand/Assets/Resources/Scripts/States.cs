06ca983e3815506f9c7ef75789ad294fc659acdf
SimonMarkWhittle <smw2ed@virginia.edu>
2018-07-14 21:53:05 -0400

    fixed typo

--- Hand to Hand/Assets/Resources/Scripts/States.cs
+++ Hand to Hand/Assets/Resources/Scripts/States.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum Action_States {
+public enum Action_State {
     Up_Idle, Low_Idle, Air_Idle, Moving_Forward, Moving_Backward,
     Up_LightPunch, Low_LightPunch, Air_LightPunch,
     Up_HeavyPunch, Low_HeavyPunch, Air_HeavyPunch,
@@ -11,11 +11,13 @@ public enum Action_States {
     Stun, Jump, Grapple
 };
 
-public enum Execution_States {
+public enum Execution_State {
     Preparing, Executing, Recoiling
 };
 
 public class States : MonoBehaviour {
+
+    Action_State action;
 
 	// Use this for initialization
 	void Start () {
